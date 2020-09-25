fc3354cf5e80188e442d96995648d269747894d6
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-25 04:31:56 -0300

    regras de pedido adicionadas

--- src/main/java/com/cliente/recepcaopedido/service/PedidoService.java
+++ src/main/java/com/cliente/recepcaopedido/service/PedidoService.java
@@ -9,7 +9,15 @@ import com.cliente.recepcaopedido.interfaces.repository.PedidoRepository;
 import com.cliente.recepcaopedido.interfaces.repository.ProdutoRepository;
 import com.cliente.recepcaopedido.interfaces.service.PedidoServiceInterface;
 import com.cliente.recepcaopedido.model.PedidoModel;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,14 +38,45 @@ public class PedidoService implements PedidoServiceInterface {
     public String adicionarPedido(List<PedidoModel> pedido) {
         if(pedido.size() >= 1 && pedido.size() <= 10) {
             for(PedidoModel result : pedido) {
-                PedidoModel pedidoConsulta = pedidoRepository.findByNumeroControle(result.getNumeroControle());
-                if(pedidoConsulta == null) { //não encontrou nenhum pedido com o numero de controle especificado
-                    pedidoConsulta = pedidoRepository.save(result);
-                    System.out.println("Pedido salvo: "+pedidoConsulta.getId());
-                }
+                System.out.println("pedidos "+pedido.size());
+                System.out.println("Pedido atual "+result.getId());
+                String data = validarData(result.getDataCadastro());
+                System.out.println("DATA "+data);
+                if(data != null) { //data  válida ou nova data caso não o tenha
+                    PedidoModel pedidoConsulta = pedidoRepository.findByNumeroControle(result.getNumeroControle());
+                    if(pedidoConsulta == null) { //não encontrou nenhum pedido com o numero de controle especificado
+                        result.setDataCadastro(data);
+                        pedidoConsulta = pedidoRepository.save(result);
+                        System.out.println("Pedido salvo: "+pedidoConsulta.getId());
+                    }
+                    System.out.println("Pedido encontrado: "+pedidoConsulta.getId());
+                } else return "Data inválida: "+result.getDataCadastro()+" formato correto: (dd/MM/yyyy) - número controle: "+result.getNumeroControle();
             }
             return "fim";
         } else return "Limite máximo de pedidos: 10! Total: "+pedido.size();
     }
     
+    /**
+     * Valida uma data
+     * Caso a data estiver nulo, retornará a data atual
+     * Caso a data não estiver nulo, validar se está no formato (dd/MM/yyyy)
+     * @param data
+     * @return 
+     */
+    private String validarData(String data) {
+        if(data != null) {
+            if(data.length() == 10) {
+                DateFormat dataObj = new SimpleDateFormat("dd/MM/yyyy");
+                dataObj.setLenient(false);
+                try {
+                    dataObj.parse(data);
+                    return data;
+                } catch (Exception ex) { return null; }
+            } else return null;
+        } else {
+            Calendar dataObj = Calendar.getInstance();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            return dateFormat.format(dataObj.getTime());
+        }
+    }
 }
