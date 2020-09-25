cc48d1dab86cd806131d2e9e592d6775a056dc1a
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-25 00:00:30 -0300

    first commit

---
+++ src/main/java/com/cliente/recepcaopedido/RecepcaopedidoApplication.java
@@ -0,0 +1,14 @@
+package com.cliente.recepcaopedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepcaopedidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecepcaopedidoApplication.class, args);
+        System.out.println("Servidor iniciado!");
+    }
+
+}
