b63c0b69a00e3a71ad7c667c4da014d78ac1a563
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-26 05:02:59 -0300

    request e response em XML

--- src/main/java/com/cliente/recepcaopedido/controller/PedidoController.java
+++ src/main/java/com/cliente/recepcaopedido/controller/PedidoController.java
@@ -11,6 +11,7 @@ import com.cliente.recepcaopedido.interfaces.service.PedidoServiceInterface;
 import com.cliente.recepcaopedido.response.AdicionarPedidoResponse;
 import com.cliente.recepcaopedido.request.PesquisarPedidoRequest;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,11 +31,22 @@ public class PedidoController {
         return pedidoInterface.adicionarPedido(pedido);
     }
     /**
-     * Pesquisa por pedidos no banco de dados
-     * @param id 
+     * Pesquisa por pedidos no banco de dados e retorna em JSON
+     * @param pesquisa objeto em JSON
+     * @return JSON
      */
-    @PostMapping("/pesquisar")
+    @PostMapping(value = "/pesquisar", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PedidoModel> pesquisar(@RequestBody PesquisarPedidoRequest pesquisa) {
+        return pedidoInterface.pesquisarPedido(pesquisa);
+    }
+    
+    /**
+     * Pesquisa por pedidos no banco de dados e retorna em XML
+     * @param pesquisa objeto em XML
+     * @return XML
+     */
+    @PostMapping(value = "/pesquisar", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
+    public List<PedidoModel> XMLpesquisar(@RequestBody PesquisarPedidoRequest pesquisa) {
         return pedidoInterface.pesquisarPedido(pesquisa);
     }
 }
